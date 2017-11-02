9462bb38f9e7cdfecd4eb97ce509669d56b4c3e6
U-PH\rodrigo.adolfo <rodrigo.adolfo@PH-0543.ph.devellar.lan>
2017-11-02 14:43:03 +0800

    Update the IP Calculator.

--- helloworld.pl
+++ helloworld.pl
@@ -1,7 +1,11 @@
-#!/usr/bin/per -w
+#!/usr/bin/perl -w
 use strict;
 use 5.010;
 
-say "Hello World!";
+state ($var1, $var2);
 
-say "Welcome to Perl";
+$var1 = "Testing";
+$var2 = "Rodrigo";
+say "Hello World $var2!";
+
+say "Welcome to Perl $var1!";
